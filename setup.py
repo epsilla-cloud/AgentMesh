4cb5ab963182933a0854aa3b337853863c726f28
Eric <eric@epsilla.com>
2025-05-10 00:17:30 -0700

    init framework
    Signed-off-by: Eric <eric@epsilla.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name="pyagentmesh",
+    version=open("./pyagentmesh/VERSION").read().strip(),
+    keywords="epsilla",
+    author="Epsilla Team",
+    author_email="info@epsilla.com",
+    description="Epsilla AgentMesh",
+    long_description="Epsilla AgentMesh",
+    license="Apache License",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["requests", "sentry_sdk", "pydantic"],
+    url="https://github.com/epsilla-cloud/agentmesh",
+    project_urls={
+        "Source": "https://github.com/epsilla-cloud/agentmesh",
+    },
+)
